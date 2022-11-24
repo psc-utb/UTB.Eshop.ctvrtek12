fc99e5ffdb5e740f38722381d01f3613b44e1451
psc-utb <psc.utb@gmail.com>
2022-11-24 13:40:57 +0100

    ASP.NET Core Identity + Instalace NuGet package pro ASP.NET Core Identity ~ Změna dědičnosti pro EshopDbContext -> IdentityDbContext + Přidání entit User, Role a enum Roles ~ Konfigurace aplikace pro použití Identity (registrace a konfigurace ve Startup.cs) + Přidána migrace pro tabulky z Identity a pro inicializaci tabulek + Přidána inicializace databáze pro nové tabulky -> přidání rolí, uživatelů a přiřazení rolí pro konkrétní uživatele + Přidána Area Security s AccountControllerem (bez implementace pro HttpPost Login, HttpPost Register a Logout, a bez DI služby) a s View pro Login a Registraci + Přidán styl login.css pro registrační a přihlašovací formuláře + Přidání LoginViewModelu a RegisterViewModelu + Přidána a nakonfigurována služba pro přihlašování a registraci + Přidání odkazů do _Layout pro nepřihlášeného a přihlášeného uživatele

--- UTB.Eshop.Web/Models/Database/EshopDbContext.cs
+++ UTB.Eshop.Web/Models/Database/EshopDbContext.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using UTB.Eshop.Web.Models.Entities;
+using UTB.Eshop.Web.Models.Identity;
 
 namespace UTB.Eshop.Web.Models.Database
 {
-    public class EshopDbContext : DbContext
+    public class EshopDbContext : IdentityDbContext<User, Role, int>
     {
         public DbSet<CarouselItem> CarouselItems { get; set; }
 
@@ -19,8 +22,24 @@ namespace UTB.Eshop.Web.Models.Database
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.GetTableName().Replace("AspNet", String.Empty));
+            }
+
             DatabaseInit databaseInit = new DatabaseInit();
             builder.Entity<CarouselItem>().HasData(databaseInit.CreateCarouselItems());
+
+            builder.Entity<Role>().HasData(databaseInit.CreateRoles());
+
+            (User admin, List<IdentityUserRole<int>> adminRole) = databaseInit.CreateAdminWithRoles();
+            (User manager, List<IdentityUserRole<int>> managerRole) = databaseInit.CreateManagerWithRoles();
+
+            builder.Entity<User>().HasData(admin, manager);
+            builder.Entity<IdentityUserRole<int>>().HasData(adminRole);
+            builder.Entity<IdentityUserRole<int>>().HasData(managerRole);
         }
     }
 }
